f36b34979371173f4f25a4d2de8e33cc33390fa0
AdmPac <for_apb2015@mail.ru>
2024-09-24 15:00:13 +0300

    added layouts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,7 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel | About</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <div class="container mt-4">
-            <h1>О блоге</h1>
-            <p>Эксперименты с Ларавелем на Хекслете</p>
-            <!-- BEGIN (write your solution here) -->
-            @foreach($team as $employee)
-                Employee: {{$employee['name']}}<br>
-                Position: {{$employee['position']}}<br>
-            @endforeach
-            <!-- END -->
-        </div>
-    </body>
-</html>
+<!-- BEGIN (write your solution here) -->
+@extends('layouts.app')
+@section('header', 'О блоге')
+
+@section('content')
+    <p>Эксперименты с Ларавелем на Хекслете</p>
+@endsection
