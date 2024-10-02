453c068b9ccc6e4ea314c481da9b43aac755e0b9
AdmPac <for_apb2015@mail.ru>
2024-10-02 13:02:33 +0300

    edited the form-methods

--- resources/views/login/create.blade.php
+++ resources/views/login/create.blade.php
@@ -105,7 +105,7 @@
                 @endforeach
               @endif
 
-              {{html()->form('POST', route('login.store'))->open()}}
+              {{Form::open(['url' => route('login.store'), 'method' => 'POST'])}}
               <input
                 name="email"
                 class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
@@ -147,7 +147,7 @@
                   Privacy Policy
                 </a>
               </p>
-              {{html()->closeModelForm()}}
+              {{Form::close()}}
             </div>
           </div>
         </div>
